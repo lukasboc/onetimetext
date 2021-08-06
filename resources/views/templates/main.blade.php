f401443030bdcdb66dacc2eabf6775e8e047e00f
Lukas Bock <lukas.bock@uni-oldenburg.de>
2021-08-06 23:59:52 +0200

    reformat code

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -1,61 +1,64 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'OneTimeText' )}}</title>
+    <title>{{ config('app.name', 'OneTimeText' )}}</title>
 
 
-        <!-- Styles -->
+    <!-- Styles -->
 
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-        <!-- JS -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- JS -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    </head>
-    <body>
-    <nav class="navbar navbar-expand-sm">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">OneTimeText</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ url('/') }}">OneTimeText</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">
                     <i style="color: white; font-size: 21pt" class="bi bi-list" id="navbar-icon"></i>
                 </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    @if (Route::has('login'))
-                        @auth
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none">
+                                @csrf
+                            </form>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none">
-                                    @csrf
-                                </form>
+                                <a class="nav-link" href="{{ route('register') }}">Register</a>
                             </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                                </li>
-                            @endif
-                        @endauth
-                    @endif
-                </ul>
-            </div>
+                        @endif
+                    @endauth
+                @endif
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    @can('logged-in')
+@can('logged-in')
     <nav class="navbar sub-nav navbar-expand-sm">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText2" aria-controls="navbarText2" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText2"
+                    aria-controls="navbarText2" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">
                     <i style="color: white; font-size: 21pt" class="bi bi-list" id="navbar-icon"></i>
                 </span>
@@ -63,9 +66,9 @@
             <div class="collapse navbar-collapse" id="navbarText2">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     @can('is-admin')
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('text.secret.index') }}">Texts</a>
                         </li>
@@ -77,16 +80,18 @@
             </div>
         </div>
     </nav>
-    @endcan
+@endcan
 
 
-    <main class="mt-5">
-        <div class="container">
-            @include('partials.alerts')
-            @yield('content')
-        </div>
-    </main>
+<main class="mt-5">
+    <div class="container">
+        @include('partials.alerts')
+        @yield('content')
+    </div>
+</main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
 </html>
