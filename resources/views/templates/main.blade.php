ca74452e7f10d68ac62971c10845fb0470fb5e9e
Lukas Bock <lukas.bock@uni-oldenburg.de>
2021-08-11 19:18:53 +0200

    changed home href for logged in users

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -74,7 +74,7 @@
                         </li>
                     @endcan
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
                     </li>
                 </ul>
             </div>
