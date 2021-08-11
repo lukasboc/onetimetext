46fc4eaa0815c57f450d96dbbc8c440af2e77335
Lukas Bock <lukas.bock@uni-oldenburg.de>
2021-08-11 19:44:19 +0200

    typo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
             </h1>
             <div class="col-lg-7 mx-auto text-lightgray">
                 <p class="fs-5 mb-4">Ein OneTimeText ist eine Nachricht, die nur einmalig abgerufen werden kann. Nach
-                    der Erstellung erhältst du einen Link, du weiterleiten kannst. Über diesen Link kann deine erstellte
+                    der Erstellung erhältst du einen Link, den du weiterleiten kannst. Über diesen Link kann deine erstellte
                     Nachricht 1x abgerufen werden, bevor sie aus dem System gelöscht wird.</p>
             </div>
 
