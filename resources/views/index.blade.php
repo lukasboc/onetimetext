2b57b5f028c97d71195b5c8d0a436faab3b3851d
Lukas Bock <lukas.bock@uni-oldenburg.de>
2021-09-01 12:41:16 +0200

    added info area to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,11 +75,39 @@
                     </form>
                 </div>
                 <div class="justify-content-center align-self-center col-8 col-sm-6 mx-auto mx-sm-0 mt-5 mt-sm-0">
-                    <img id="index-image" src="{{asset('images/mobilundraw_Safe_re_kiil-3220.png')}}" alt="image">
+                    <img class="index-image" src="{{asset('images/mobilundraw_Safe_re_kiil-3220.png')}}" alt="image">
                 </div>
             </div>
         </div>
     </div>
+
+    <section id="about">
+    <div class="container mt-5">
+        <header class="text-center mb-5">
+            <h5 class="text-uppercase">Über</h5>
+            <h1>Informationen zu <span class="highlight-text">OneTimeText</span>.</h1>
+            <p>In diesem Bereich finden Sie Informationen dazu, wozu dieses Tool dient.</p>
+        </header>
+        <div class="row mb-5">
+            <div class="col-sm-8 col-md-6 justify-content-center align-self-center">
+                <h1>Was ist OneTimeText?</h1>
+                <p>OneTimeText ist ein Werkzeug, das Ihnen dabei helfen soll, sensible Daten wie z.B. Passwörter zu versenden, ohne dass diese in Chat- oder E-Maiverläufen auftauchen. Sie können den Text, den Sie mit jemandem teilen möchten einfach in eine OneTimeText-Nachricht verpacken und den Link versenden. Der von Ihnen verpackte Text, der ausschließlich durch diesen Link aufrufbar ist, kann nur 1x gelesen werden, bevor die Nachricht aus System gelöscht wird. Sie brauchen sich also keine Gedanken darüber zu machen, dass weitere Personen auf die geheime Nachricht zugreifen können.</p>
+            </div>
+            <div class="text-center justify-content-center align-self-center col-8 col-sm-4 col-md-6 mx-auto mx-sm-0 mt-5 mt-sm-0">
+                <img src="{{asset('images/whatis.png')}}" alt="image" class="img-fluid index-image" id="what-is-image">
+            </div>
+        </div>
+        <div class="row">
+            <div class="text-center justify-content-center align-self-center col-8 col-sm-4 col-md-6 mx-auto mx-sm-0 mt-5 mt-sm-0 d-none d-sm-block">
+                <img src="{{asset('images/how.png')}}" alt="image" class="img-fluid index-image" id="how-image">
+            </div>
+            <div class="col-sm-8 col-md-6 justify-content-center align-self-center">
+                <h1>Wie benutze ich OneTimeText?</h1>
+                <p>Die Nutzung von OneTimeText könnte nicht einfacher sein. Sie benötigen keinen Benutzeraccount um dieses Tool nutzen zu können. Geben Sie einfach Ihren geheimen Text in das Feld auf der Startseite ein und klicken Sie auf "Link erstellen". Kopieren Sie den Link und versenden Sie diesen an den Empfänger per Mail, Signal, Whatsapp oder auch per Brief. Der Nachricht-Empfänger muss zum lesen der Nachricht lediglich auf den Link klicken und auf den Button "OneTimeText öffnen" klicken.</p>
+            </div>
+        </div>
+    </div>
+    </section>
 
 
 @endsection
