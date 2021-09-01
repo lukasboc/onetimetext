3730905e9a6f10a1510e2d147c0185c7b5a307b3
Lukas Bock <lukas.bock@uni-oldenburg.de>
2021-09-01 13:20:41 +0200

    added imprint text

--- resources/views/legal/imprint.blade.php
+++ resources/views/legal/imprint.blade.php
@@ -4,11 +4,25 @@
     <div class="px-4 py-5">
         <div class="py-5">
             <h1 class="display-5 fw-bold text-white">Impressum.</h1>
-            <div class="col-lg-7 mx-auto text-lightgray">
-                <p class="fs-5 mb-4">Ein OneTimeText ist eine Nachricht, die nur einmalig abgerufen werden kann. Nach
-                    der Erstellung erhältst du einen Link, den du weiterleiten kannst. Über diesen Link kann deine erstellte
-                    Nachricht 1x abgerufen werden, bevor sie aus dem System gelöscht wird.</p>
+            <div class="text-lightgray">
+                    <h3>Angaben gemäß § 5 TMG</h3>
+                    <p>Lukas Bock <br> In den Breden 20<br> 27386 Bothel <br>
+                    </p>
+                    <h3> Vertreten durch:</h3> <p>Lukas Bock<br>
+                    </p>
+                    <h3>Kontakt:</h3><p> Telefon: 0441 59473836<br> E-Mail: <a href="mailto:info@lubomedia.de?subject=Kontaktanfrage">info@lubomedia.com</a>
+                    </p>
+                    <h3>Steuernummer: </h3><p> 40/105/05213
+                    </p>
+                    <h3>Umsatzsteuer: </h3><p> Gemäß § 19 UStG wird keine Umsatzsteuer berechnet bzw. ausgewiesen.
+                    </p>
+                    <h3>Aufsichtsbehörde:</h3><p> Finanzamt Rotenburg Wümme</p>
+                    <h3>Haftungsausschluss: </h3>
+                    <h3>Urheberrecht</h3>
+                    <p>Die durch den Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitte ich um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Inhalte umgehend entfernen.</p>    <p></p>
             </div>
+        </div>
+    </div>
 
 
 @endsection
--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -4,11 +4,12 @@
     <div class="px-4 py-5">
         <div class="py-5">
             <h1 class="display-5 fw-bold text-white">Datenschutz.</h1>
-            <div class="col-lg-7 mx-auto text-lightgray">
-                <p class="fs-5 mb-4">Ein OneTimeText ist eine Nachricht, die nur einmalig abgerufen werden kann. Nach
+            <div class="text-lightgray">
+                <p>Ein OneTimeText ist eine Nachricht, die nur einmalig abgerufen werden kann. Nach
                     der Erstellung erhältst du einen Link, den du weiterleiten kannst. Über diesen Link kann deine erstellte
                     Nachricht 1x abgerufen werden, bevor sie aus dem System gelöscht wird.</p>
             </div>
-
+        </div>
+    </div>
 
 @endsection
